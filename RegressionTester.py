17a6caa38459f12031af9ad9d1a8112caa30181c
brucecurcio <brucecurcio@gmail.com>
2017-08-25 07:00:22 -0700

    2nd Commit

--- RegressionTester.py
+++ RegressionTester.py
@@ -6,6 +6,16 @@ if __name__ == "__main__":
 
     with open('amzn_hist_test.csv') as csvfile:
         priceList = csv.DictReader(csvfile)
-        #pprint(priceList.fieldnames)
-        for row in priceList:
-            print(row)
+        #for row in priceList:
+        #   print(row)
+
+    with open ('Option_Exp_dates_Test.csv') as csvfile2:
+        optionExp = csv.DictReader(csvfile2)
+        for row in optionExp:
+            pprint(row)
+
+    #pprint(optionExp['TradeDate'])
+    #startDate=optionExp['TradeDay']
+    #pprint(startDate)
+
+    #why is this modifying my OptionExp csv, merging cell date
