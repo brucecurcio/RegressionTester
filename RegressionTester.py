7c13c0158ecc2a9975648c01416571c8853b9cad
brucecurcio <brucecurcio@gmail.com>
2017-09-18 18:05:23 -0700

    18SEP17

--- RegressionTester.py
+++ RegressionTester.py
@@ -24,7 +24,7 @@ def calc_strike(lowPrice):
 
 
 if __name__ == "__main__":
-
+    #pprint('start')
     with open('amzn_hist_test.csv', 'r') as csvfile:
         priceIter = csv.DictReader(csvfile)
         priceList = list(priceIter)     #convert iterable into list of dictionaries
@@ -44,36 +44,43 @@ if __name__ == "__main__":
 
     # find the first trading day after the first exp
     pl_counter=0
+
     while pl_counter < priceListLen:
+
         if startDate != convert_date(priceList[pl_counter]['date']):
             pl_counter += 1
+        else:
+            break
 
     ownStock = 'no'
     acct_balance = 0
 
-
+    #pprint(pl_counter)
     exp_counter=1
-    while (exp_counter <= optListLen): #iterate through expiration dates in csv
+    while exp_counter <= optListLen: #iterate through expiration dates in csv
+        #pprint(exp_counter)
         nextExpDay=convert_date(optionExpList[exp_counter]['Exp Day']) #get the next expiration day
-        account_balance = acct_balance+premium_collected(convert_date(priceList[pl_counter]['low'])# collect premium for sale of put
-        strikePrice=calc_strike(convert_date(priceList[pl_counter]['low']) #capture the closest strike price
+        sellPrem=premium_collected(priceList[pl_counter]['low'])
+        acct_balance += sellPrem # collect premium for sale of put
+        strikePrice=calc_strike(priceList[pl_counter]['low']) #capture the closest strike price
 
         while convert_date(priceList[pl_counter]['date']) <= nextExpDay:
-            #pprint(convert_date(priceList[pl_counter]['date']) <= nextExpDay)
-            if float(priceList[pl_counter]['low']) < putStrike * .9:
+            #pprint (convert_date(priceList[pl_counter]['date']))
+            if float(priceList[pl_counter]['low']) < strikePrice * .9:
                 # buy put back, subtract premium+10% loss; need a better estimate here; will tune so that losses will decrease
                 # as we get closer to expiration; maybe 10% to start and then decrease to 1% as we get closer to expiration
                 acct_balance=acct_balance-2*sellPrem
+                if convert_date(priceList[pl_counter]['date'])-nextExpDay < 16: #move to next exp date if less than 15 days til expire on current month
+                    exp_counter+=1
                 break
             else:
                 pl_counter+=1
-                #pprint(pl_counter)
-                pprint(convert_date(priceList[pl_counter]['date']))
-        exp_counter+=1
+                if convert_date(priceList[pl_counter]['date'])== nextExpDay: #move to next exp date if iterated through current month
+                    pprint (convert_date(priceList[pl_counter]['date'])== nextExpDay)
+                    exp_counter+=1
 
 
-    #exp_counter=+1
-    #nextExpDay=nextExpDay+timedelta(days=1)
-    #pprint(nextExpDay)
+
+
 
 
